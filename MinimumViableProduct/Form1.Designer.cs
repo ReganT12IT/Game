5577a4b3ca3d219d2db00f9b92cffd613a6f2c0c
1rtunsta <1rtunsta@A6-WSA7-B360>
2024-05-22 13:30:21 +1200

    Add project files.

---
+++ MinimumViableProduct/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MinimumViableProduct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(72, 405);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(28, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.player_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Location = new System.Drawing.Point(40, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(761, 458);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp_1);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MinimumViableProduct/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinimumViableProduct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Declare variables
+        bool right, left;
+        private void player_Click(object sender, EventArgs e)
+        {
+
+        }
+        //When key pressed
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            //Move left and right
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (right == true) { player.Left += 5; }//5 pixels to the right
+            if (left == true) { player.Left -= 5; }//5 pixels to the left
+            //Stop playing moving left
+            if (!player.Bounds.IntersectsWith(panel1.Bounds.Left()))
+            {
+                left = false;
+                if
+                    {
+                    player.Left += 1;
+                }
+            }
+        }
+
+        //When key released
+        private void Form1_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            //Stop moving left and right
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+        }
+    }
+}
